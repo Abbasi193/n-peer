3826e1c178dc634c781c26c4f8ea74665c4563ff
Abbasi193 <shahidabbasi3625@gmail.com>
2021-03-15 00:49:08 +0500

    make it better5

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const cors = require('cors');
 const express = require('express')
@@ -9,11 +9,7 @@ var cookieParser = require('cookie-parser')
 const { ExpressPeerServer } = require('peer');
 const app = express()
 
-const sslOptions = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-const server = https.createServer(sslOptions, app)
+const server = http.createServer(app)
 const io = require('socket.io')(server)
 const JWT_SECRET = 'b2637fe72f618333ce1c0326b09c526e37c785be585695f5617a2e95ac96cf8f8a4d39749b257d3bf1c52e356e8b0486c5f3d60fc3aaab0cb573fe9d0787f6b1';
 const uri = 'mongodb+srv://abbasiMongoDb:abbasiMongoDb@cluster0.s0lk6.gcp.mongodb.net/mobiledb?retryWrites=true&w=majority';

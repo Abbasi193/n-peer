68368be42a453ce27ae7a4b3be54a31bd91f5849
Abbasi193 <48798551+Abbasi193@users.noreply.github.com>
2022-07-27 06:36:59 +0500

    fix server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require('socket.io')(server)
 const JWT_SECRET = 'b2637fe72f618333ce1c0326b09c526e37c785be585695f5617a2e95ac96cf8f8a4d39749b257d3bf1c52e356e8b0486c5f3d60fc3aaab0cb573fe9d0787f6b1';
-const uri = 'mongodb+srv://abbasiMongoDb:abbasiMongoDb@cluster0.s0lk6.gcp.mongodb.net/mobiledb?retryWrites=true&w=majority';
+const uri = PROCESS.ENV.URL;
 const User = require('./model/user');
 const Group = require('./model/group');
 const Message = require('./model/message');

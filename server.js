866ae500ce59f84fc6537bff2d7215d164d97d63
Abbasi193 <48798551+Abbasi193@users.noreply.github.com>
2022-07-27 07:09:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 const server = http.createServer(app)
 const io = require('socket.io')(server)
-const JWT_SECRET = 'b2637fe72f618333ce1c0326b09c526e37c785be585695f5617a2e95ac96cf8f8a4d39749b257d3bf1c52e356e8b0486c5f3d60fc3aaab0cb573fe9d0787f6b1';
+const JWT_SECRET = PROCESS.ENV.SECRET;
 const uri = PROCESS.ENV.URL;
 const User = require('./model/user');
 const Group = require('./model/group');

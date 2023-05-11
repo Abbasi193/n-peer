c3f941fc5514d8d1f817323a17783ed53181cd42
Abbasi193 <shahidabbasi3625@gmail.com>
2023-05-11 15:21:43 +0500

    scan

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const jwt = require('jsonwebtoken')
 var cookieParser = require('cookie-parser')
 const { ExpressPeerServer } = require('peer');
 const app = express()
-const JWT_SECRET = 'b2637fe72f618333ce1c0326b09c526e37c785be585695f5617a2e95ac96cf8f8a4d39749b257d3bf1c52e356e8b0486c5f3d60fc3aaab0cb573fe9d0787f6b1';
 const server = http.createServer(app)
 const io = require('socket.io')(server)
 const JWT_SECRET = PROCESS.ENV.SECRET;

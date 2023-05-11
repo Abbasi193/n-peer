05c6c486a677a4eb1a2c55f1902ecb17b2c3f2e8
Abbasi193 <shahidabbasi3625@gmail.com>
2023-05-11 17:42:52 +0500

    scan

--- scan.sh
+++ scan.sh
@@ -1,6 +1,6 @@
 echo '----------------------------Dependency-Testing----------------------------'
 snyk test || echo -e "\033[0;31mDependency-Testing failed with error code $?.\033[0m"
-echo '----------------------------Code-Testing----------------------------------'
-snyk code test || echo -e "\033[0;31mCode-Testing failed with error code $?.\033[0m"
+# echo '----------------------------Code-Testing----------------------------------'
+# snyk code test || echo -e "\033[0;31mCode-Testing failed with error code $?.\033[0m"
 echo '----------------------------Monitor---------------------------------------'
 snyk monitor || echo -e "\033[0;31mTMonitoring failed with error code $?.\033[0m"
